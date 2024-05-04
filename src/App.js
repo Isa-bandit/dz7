16cebc9e129fb94f62d6e912847bf5bac32b985a
Isa-bandit <128226535+Isa-bandit@users.noreply.github.com>
2024-05-04 23:54:12 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import TodosPage from "./pages/TodosPage";
+import './App.css'
+
+
+function App() {
+
+
+    return (
+        <div>
+            <TodosPage/>
+
+        </div>
+    )
+
+}
+
+export default App;
